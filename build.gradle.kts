300190d3f229d1154ad9aefb553860b3c2616279
Matthias Raab <m.raab@intershop.de>
2020-09-16 12:06:53 +0200

    add exclude

--- build.gradle.kts
+++ build.gradle.kts
@@ -239,9 +239,9 @@ bintray {
 
 dependencies {
     shaded("jakarta.ws.rs:jakarta.ws.rs-api:2.1.6")
-    shaded("org.glassfish.jersey.core:jersey-client:2.29.1") /** {
+    shaded("org.glassfish.jersey.core:jersey-client:2.29.1") {
         exclude(group = "org.glassfish.hk2.external", module="jakarta.inject")
-    } */
+    }
     shaded("org.glassfish.jersey.media:jersey-media-json-jackson:2.29.1")
     shaded("org.codehaus.jettison:jettison:1.4.1")
 
